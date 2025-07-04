9770dd1a3dad5be1d980f56991b3a4e83b5b0eb3
Sherrlyn Anndrina J <154114732+sherrlyn18@users.noreply.github.com>
2025-07-04 23:49:30 +0530

    Update diceGame.js

--- diceGame/diceGame.js
+++ diceGame/diceGame.js
@@ -1,74 +1,100 @@
-// Select elements
-const playerEls = [document.getElementById('player1'), document.getElementById('player2')];
-const scoreEls = document.querySelectorAll('.score');
-const currentEls = document.querySelectorAll('.c-score');
-const diceEl = document.querySelector('.dice');
+// const player1 = document.getElementById('#player1');
+// const player2 = document.getElementById('#player2');
+const score1 = document.querySelector('#score1');
+const score2 = document.querySelector('#score2');
+const currscore1 = document.querySelector('#c-score1');
+const currscore2 = document.querySelector('#c-score2');
+const dice = document.querySelector('.dice');
 const rollBtn = document.querySelector('.rolldice');
 const holdBtn = document.querySelector('.hold');
 const newGameBtn = document.querySelector('.newgame');
+const result = document.querySelector('.result');
+const currplayer = document.querySelector('.currplayer');
 
-let scores, currentScore, activePlayer, playing;
 
-const init = () => {
-    score1= 0;
-    score2=0;
-    currentScore = 0;
-    activePlayer = 0;
-    playing = true;
+let currscore = 0;
+let activeplayer=1;
+let total1 = 0;
+let total2 =0;
+let winscore=20
 
-    scoreEls.forEach(el => el.textContent = '0');
-    currentEls.forEach(el => el.textContent = '0');
+console.log(currscore);
+console.log(newGameBtn);
 
-    diceEl.style.display = 'none';
+const newgame = () => {
+    console.log("newgame");
+    total1=0;
+    total2=0;
+    currscore=0;
+    activeplayer=1;
+    currscore1.textContent = "0";
+    currscore2.textContent = "0";
+    score1.textContent = "0";
+    score2.textContent = "0";
+    dice.src = "/diceGame/dice1.jpg";
+    result.textContent=" "
+    //dice.style.display = "none";
+    rollBtn.style.display = "block";
+    holdBtn.style.display = "block";
+}
 
-    playerEls[0].classList.add('active');
-    playerEls[1].classList.remove('active');
-    playerEls.forEach(player => player.classList.remove('winner'));
-};
 
-const switchPlayer = () => {
-    currentEls[activePlayer].textContent = '0';
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
+const hold = () => {
+    if(activeplayer===1) {
+        currscore1.textContent="0";
+        activeplayer=2;
+        console.log(activeplayer);
+    } else {
+        currscore2.textContent="0";
+        activeplayer=1;
+    }
+}
 
-    playerEls[0].classList.toggle('active');
-    playerEls[1].classList.toggle('active');
-};
 
-// Roll Dice
-rollBtn.addEventListener('click', function () {
-    if (playing) {
-        const dice = Math.trunc(Math.random() * 6) + 1;
 
-        diceEl.src = `/diceGame/dice${dice}.jpg`;
-        diceEl.style.display = 'block';
+const rolldice = setInterval(() => {
+//const rolldice = () => {
+    const dice_num = Math.trunc(Math.random()*6)+1;
+    dice.style.display = "block";
+    dice.src = `/diceGame/dice${dice_num}.jpg`;
+    currscore = dice_num;
+    console.log("currscore: " +currscore);
+    if(currscore===1){
+        currscore=0;
+        hold();
+    }
+    
+    if(activeplayer===1) {
+        total1+=currscore;
+        console.log("TotalSCore: "+ total1);
+        
+        currscore1.textContent = dice_num;
+        score1.textContent = total1;
 
-        if (dice !== 1) {
-            currentScore += dice;
-            currentEls[activePlayer].textContent = currentScore;
-        } else {
-            switchPlayer();
+        if(total1>=winscore){
+            holdBtn.style.display="none";
+            rollBtn.style.display="none";
+            clearInterval(rolldice);
+            result.textContent="Player 1 WIN";
+        }
+
+    } else {
+        total2+=currscore;
+        currscore2.textContent = dice_num;
+        score2.textContent = total2;
+
+        if(total2>=winscore){
+            rollBtn.style.display="none";
+            holdBtn.style.display="none";
+            currplayer.style.display="none"
+            clearInterval(rolldice)
+            result.textContent="Player 2 WIN";
         }
     }
-});
 
-// Hold Score
-holdBtn.addEventListener('click', function () {
-    if (playing) {
-        scores[activePlayer] += currentScore;
-        scoreEls[activePlayer].textContent = scores[activePlayer];
+},3000)
 
-        if (scores[activePlayer] >= 100) {
-            playing = false;
-            playerEls[activePlayer].classList.add('winner');
-            diceEl.style.display = 'none';
-        } else {
-            switchPlayer();
-        }
-    }
-});
 
-// New Game
-newGameBtn.addEventListener('click', init);
-
-init(); // initialize the game
+newGameBtn.addEventListener("click", newgame);
+rollBtn.addEventListener("click", rolldice);
+holdBtn.addEventListener("click", hold);
